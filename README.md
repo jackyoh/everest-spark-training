01dfeef2bfb9817426212c0f70b2eba5fdd7b166
AllenFang <ayu780129@hotmail.com>
2016-07-05 11:36:28 +0800

    init repo

---
+++ README.md
@@ -0,0 +1,26 @@
+# everest-spark-training
+Apache Spark training for EVEREST
+
+Clone or Download Project
+```bash
+$ git clone https://github.com/AllenFang/everest-spark-training.git
+```
+
+Build
+```bash
+$ cd everest-spark-training
+$ ./gradlew clean build -x test
+$ ./gradlew eclipse
+```
+
+project import to eclipse
+```
+File->
+	Import->
+		General->
+			Existing Projects into Workspace->
+				Next->
+					Browser->
+						everest-spark-training folder->
+							Finish
+```
---
+++ src/main/java/com/spark/example/HelloSpark.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package com.spark.example;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function2;
+
+/**
+ * @author allen
+ *
+ */
+public class HelloSpark {
+
+	/**
+	 * 
+	 */
+	public HelloSpark() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SparkConf conf = new SparkConf().setAppName("Hello_World").setMaster("local");
+		JavaSparkContext sc = new JavaSparkContext(conf);
+		
+		
+		List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
+		
+		JavaRDD<Integer> distData = sc.parallelize(data);
+		
+		int result = distData.reduce(new Function2<Integer, Integer, Integer>(){
+
+			@Override
+			public Integer call(Integer v1, Integer v2) throws Exception {
+				return v1 + v2;
+			}
+			
+		});
+		
+		System.out.println(result);
+	}
+
+}
